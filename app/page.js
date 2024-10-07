2d8707cc03ef9a0b36a66a6b9989da4ba30fc91e
sol <nathf1005@naver.com>
2024-10-07 18:18:32 +0900

    Next.js에서 MongoDB 사용하기

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { connectDB } from "@/util/database";
 
-export default function Home() {
+export default async function Home() {
+  const client = await connectDB;
+  const db = client.db("forum")
+  let result = await db.collection('post').find().toArray()
+  console.log(result)
   return (
     <div>안녕</div>
   );
