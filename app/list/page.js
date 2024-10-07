065f36104f2d8eb66d24d65dad7bb0a4bd7cc4e7
sol <nathf1005@naver.com>
2024-10-07 19:51:15 +0900

    상세페이지 만들기 1 (Dynamic route)

--- app/list/page.js
+++ app/list/page.js
@@ -1,11 +1,11 @@
 import { connectDB } from "@/util/database";
+import Link from "next/link";
 
 export default async function List() {
 
     const client = await connectDB;
     const db = client.db("forum")
     let result = await db.collection('post').find().toArray()
-    console.log('result', result)
     return (
         <div className="list-bg">
             {
@@ -13,6 +13,7 @@ export default async function List() {
                     return (
                         <div className="list-item" key={i}>
                             <h4>{v.title}</h4>
+                            <Link href={`/detail/${v._id}`}>링크</Link>
                             <p>{v.content}</p>
                         </div>
                     )
