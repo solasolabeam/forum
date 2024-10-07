a09770b235c4393dbbb6842285e99b7d8d25de07
sol <nathf1005@naver.com>
2024-10-07 23:13:43 +0900

    수정기능 만들기 2

--- app/edit/[id]/page.js
+++ app/edit/[id]/page.js
@@ -9,7 +9,7 @@ export default async function Edit(props) {
     return (
         <div className="p-20">
             <h4>수정페이지</h4>
-            <form action="/api/post/modi" method="POST">
+            <form action="/api/post/edit" method="POST">
                 <input name="id" type="hidden" defaultValue={result._id} />
                 <input name="title" placeholder="글제목" defaultValue={result.title} />
                 <input name="content" placeholder="글내용" defaultValue={result.content} />
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -20,7 +21,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
+        <div className="navbar">
+          <Link href="/" className="logo">Appleforum</Link>
+          <Link href="/list">List</Link>
+        </div>
         {children}
       </body>
     </html>
