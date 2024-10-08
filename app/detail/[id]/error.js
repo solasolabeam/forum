ca19cb507457dbd63d4b269b6d40fdb43597638b
sol <nathf1005@naver.com>
2024-10-08 23:47:47 +0900

    loading.js, error.js, not-found.js

---
+++ app/detail/[id]/error.js
@@ -0,0 +1,9 @@
+'use client'
+export default function Error({ error, reset }) {
+    return (
+        <div>
+            <h4>에러남</h4>
+            <button onClick={()=>{reset()}}>버튼</button>
+        </div>
+    )
+}
--- app/detail/[id]/page.js
+++ app/detail/[id]/page.js
@@ -1,14 +1,17 @@
 import { connectDB } from "@/util/database";
 import { ObjectId } from "mongodb";
 import Comment from "./Comment";
+import { notFound } from "next/navigation";
 
 export default async function Detail(props) {
     const client = await connectDB;
     const db = client.db("forum")
     let result = await db.collection('post').findOne({ _id: new ObjectId(props.params.id) })
-
+    if(result === null){
+        return notFound()
+    }
     return (
-        <div>
+        <div className="p-20 detail">
             <h4>상세페이지임</h4>
             <h4>{result.title}</h4>
             <p>{result.content}</p>
