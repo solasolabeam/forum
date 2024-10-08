24a6de8b781f43a8edd20f59043623eabb53417e
sol <nathf1005@naver.com>
2024-10-08 23:23:59 +0900

    댓글기능 만들기 3-2

--- app/detail/[id]/Comment.js
+++ app/detail/[id]/Comment.js
@@ -4,13 +4,23 @@ import { useEffect, useState } from "react"
 
 export default function Comment({ _id }) {
     const [comment, setComment] = useState('')
+    const [data, setData] = useState('')
 
     useEffect(() => {
-        fetch('/').then(res=>res.json()).then(data=>{console.log(123)})
+        fetch(`/api/comment/list?id=${_id}`).then(res => res.json()).then(data => { setData(data) })
     }, [])
     return (
         <div>
             <div>댓글목록보여줄부분</div>
+            {
+                data.length > 0 ?
+                data.map((v, i) => {
+                    return (
+                        <p key={i}>{v.content}</p>
+                    )
+                })
+                : <p>댓글없음</p>
+            }
             <input onChange={(e) => { setComment(e.target.value) }} />
             <button onClick={() => {
                 fetch('/api/comment/new', {
