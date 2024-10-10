6f6c588989d6b1a78356a80ba463cdc18eb4a017
sol <nathf1005@naver.com>
2024-10-10 18:31:13 +0900

    서버기능 중간에 간섭하려면 Middleware

--- app/DarkMode.js
+++ app/DarkMode.js
@@ -1,13 +1,20 @@
 'use client'
 import { useRouter } from "next/navigation"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export default function DarkMode() {
+    const [mode, setMode] = useState('🌙')
     let router = useRouter()
     useEffect(() => {
         let 쿠키값 = ('; ' + document.cookie).split(`; mode=`).pop().split(';')[0]
         if (쿠키값 == '') {
             document.cookie = 'mode=light; max-age=' + (3600 * 24 * 400)
+        }
+
+        if (쿠키값 == 'dark') {
+            setMode('🌙')
+        } else {
+            setMode('☀️')
         }
     }, [])
     return (
@@ -15,12 +22,14 @@ export default function DarkMode() {
             let 쿠키값 = ('; ' + document.cookie).split(`; mode=`).pop().split(';')[0]
             if (쿠키값 == 'light') {
                 document.cookie = 'mode=dark; max-age=' + (3600 * 24 * 400)
+                setMode('🌙')
                 router.refresh()
             } else {
                 document.cookie = 'mode=light; max-age=' + (3600 * 24 * 400)
+                setMode('☀️')
                 router.refresh()
             }
-            
-        }}> 🌙 </span>
+
+        }}> {mode} </span>
     )
 } 
---
+++ middleware.js
@@ -0,0 +1,31 @@
+import { getToken } from "next-auth/jwt"
+import { NextResponse } from "next/server"
+
+export async function middleware(request) {
+    // console.log('request.nextUrl', request.nextUrl)
+    // console.log('request.cookies', request.cookies)
+    // console.log('request.headers', request.headers.get('user-agent'))
+    // NextResponse.next()
+    // NextResponse.redirect()
+    // NextResponse.rewrite()
+
+    const session = await getToken({ req: request })
+    console.log('session', session)
+
+    // if(request.nextUrl.pathname === '/list') {
+    //     console.log(request.headers.get('sec-ch-ua-platform'))
+    //     return NextResponse.next()
+    // }
+    if (request.nextUrl.pathname.startsWith('/write')) {
+        if(session == null) {
+            
+            // return NextResponse.redirect(new URL('/api/auth/signin'), request.url)
+            return NextResponse.redirect('http://localhost:3000/api/auth/signin')
+        }
+    }
+
+    if (request.nextUrl.pathname.startsWith('/list')) {
+        console.log(request.headers.get('sec-ch-ua-platform'))
+        return NextResponse.next()
+    }
+}
